830f3e197120cbd57ecb6cb8c3e0c85d88c1d81e
Sameer <109897419+Sameer-8080@users.noreply.github.com>
2023-03-23 19:18:36 +0530

    Create add.c

---
+++ add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+
+    int number1=10, number2=20, sum=0;
+
+    //printf("Enter two integers: ");
+    //scanf("%d %d", &number1, &number2);
+
+    //calculate the sum
+    sum = number1 + number2;
+
+    //printf("%d + %d = %d", number1, number2, sum);
+    printf(sum);
+    return 0;
+}
